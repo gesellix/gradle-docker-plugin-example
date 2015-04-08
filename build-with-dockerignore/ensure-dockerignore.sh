1d5ef43de224c609e84872a5f9eee41c5349c841
Tobias Gesellchen <tobias@gesellix.de>
2015-04-08 22:18:47 +0200

    update plugin dependency, show how to dockerignore complete folders

--- build-with-dockerignore/ensure-dockerignore.sh
+++ build-with-dockerignore/ensure-dockerignore.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 [   ! -e "./file-to-be-ignored" \
- -a ! -e "./subdirectory/another-file-to-be-ignored" ] || exit 1
+ -a ! -e "./subdirectory/another-file-to-be-ignored" \
+ -a ! -d "./build" \
+ -a ! -d "./ignoreddirectory" ] || exit 1
