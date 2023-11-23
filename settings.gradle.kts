57e6fa94231e8febaf5f88cc9264720c24bbf0b1
Tobias Gesellchen <tobias@gesellix.de>
2023-11-23 21:21:06 +0100

    Add a minimal example for 'docker build'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,14 @@ include(
   "build-with-auth",
   "build-with-buildargs",
   "build-with-dockerignore",
+  "build-with-tag",
   "run-exec-and-copy-locally",
   "run-with-data-volume",
   "push-and-pull-with-auth",
   "build-push-and-run-remotely",
   "issue-41",
   "issue-64",
-  "run-with-user"
+  "run-with-user",
 )
 
 //pluginManagement {
